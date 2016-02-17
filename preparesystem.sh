92f60baf59c197b92a4dd7af4d623abbeb600275
JBlond <leet31337@web.de>
2016-02-17 10:02:21 +0100

    say yes

--- preparesystem.sh
+++ preparesystem.sh
@@ -2,5 +2,5 @@
 mkdir $HOME/apache24
 cd $HOME/apache24
 sudo aptitude update || echo aptitude update hit problems but continuing anyway...
-sudo aptitude install build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev subversion
+sudo aptitude -y install build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev subversion
 svn checkout http://svn.apache.org/repos/asf/httpd/mod_fcgid/trunk mod_fcgid
