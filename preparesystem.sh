1898d7078f5066a22e1feb25073b6e4e714ef71e
JBlond <leet31337@web.de>
2016-05-10 15:20:00 +0200

    unify prepare

--- preparesystem.sh
+++ preparesystem.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 mkdir $HOME/apache24
 cd $HOME/apache24
-sudo aptitude update || echo aptitude update hit problems but continuing anyway...
-sudo aptitude -y install build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev subversion
+
+# debian
+which apt-get  > /dev/null 2>&1 && {
+	sudo aptitude update || echo aptitude update hit problems but continuing anyway...
+	sudo aptitude -y install build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev subversion
+}
+
+# centos
+which yum  > /dev/null 2>&1 && {
+	sudo yum groupinstall "Development Tools" "Development Libraries"
+	sudo yum install libtool lua-devel patch pcre-devel python-devel wget zlib-devel
+}
+
+# opensuse
+which zypper  > /dev/null 2>&1 && {
+	sudo zypper install --type pattern devel_basis
+	sudo zypper install autoconf libnghttp2-devel libtool libxml2-devel lua-devel pcre3-devel python-devel subversion zlib-devel
+}
+
+
 svn checkout http://svn.apache.org/repos/asf/httpd/mod_fcgid/trunk mod_fcgid
