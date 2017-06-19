db8c892900eee70aac4036fb3858f10161cf618f
JBlond <leet31337@web.de>
2017-06-19 15:24:21 +0200

    Add update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf "${HOME}/apache24"
+git pull
+./uninstall_daemon.sh
+./preparesystem.sh
+./build_apache.sh
+./install_as_daemon.sh
+sudo rm -rf /opt/apache2/conf/httpd.conf.bak
+sudo rm -rf /opt/apache2/conf/original
+sudo /opt/apache2/bin/httpd -k stop
+sleep 5
+sudo /opt/apache2/bin/httpd -k start
