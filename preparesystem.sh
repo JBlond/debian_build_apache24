a7bcff2668e32ee52f5aba2a3a9d6b45eef822b3
Mario <JBlond@users.noreply.github.com>
2017-10-17 18:06:51 +0200

    Build failed on ubuntu and centos.

--- preparesystem.sh
+++ preparesystem.sh
@@ -5,19 +5,19 @@ cd $HOME/apache24
 # debian
 which apt-get  > /dev/null 2>&1 && {
 	sudo aptitude update || echo aptitude update hit problems but continuing anyway...
-	sudo aptitude -y install build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev subversion
+	sudo aptitude -y install build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev subversion libexpat1-dev
 }
 
 # centos
 which yum  > /dev/null 2>&1 && {
 	sudo yum groupinstall "Development Tools" "Development Libraries"
-	sudo yum install libtool lua-devel patch pcre-devel python-devel wget zlib-devel
+	sudo yum install libtool lua-devel patch pcre-devel python-devel wget zlib-devel expat-devel
 }
 
 # opensuse
 which zypper  > /dev/null 2>&1 && {
 	sudo zypper install --type pattern devel_basis
-	sudo zypper install autoconf libnghttp2-devel libtool libxml2-devel lua-devel pcre3-devel python-devel subversion zlib-devel
+	sudo zypper install autoconf libnghttp2-devel libtool libxml2-devel lua-devel pcre3-devel python-devel subversion zlib-devel libexpat1-dev
 }
 
 
