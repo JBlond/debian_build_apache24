16df5195b5a6dd1093006b17ee82caa742bee306
Mario <JBlond@users.noreply.github.com>
2019-08-16 13:19:30 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ git pull
 ./install_as_daemon.sh
 sudo rm -rf /opt/apache2/conf/httpd.conf.bak
 sudo rm -rf /opt/apache2/conf/original
+sudo rm -rf /opt/apache2/conf/extra
 sudo /opt/apache2/bin/httpd -k stop
 sleep 5
 sudo /opt/apache2/bin/httpd -k start
