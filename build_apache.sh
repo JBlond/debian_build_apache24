275acd2c01dcc04b9724c358512cd23f3d1384cf
JBlond <leet31337@web.de>
2020-07-31 10:36:52 +0200

    remove no longer supported openssl option

--- build_apache.sh
+++ build_apache.sh
@@ -28,7 +28,7 @@ then
 	wget https://www.openssl.org/source/${SSL_FILE}
 	tar xfz ${SSL_FILE}
 	cd openssl-${SSL_VERSION}
-	./config --prefix=/opt/openssl --openssldir=/opt/openssl no-blowfish no-ssl3 no-ec2m no-rc5 no-idea no-camellia no-des no-weak-ssl-ciphers enable-ec_nistp_64_gcc_128 threads zlib-dynamic shared
+	./config --prefix=/opt/openssl --openssldir=/opt/openssl no-ssl3 no-ec2m no-rc5 no-idea no-camellia no-des no-weak-ssl-ciphers enable-ec_nistp_64_gcc_128 threads zlib-dynamic shared
 	make depend
 	make
 	sudo make install
