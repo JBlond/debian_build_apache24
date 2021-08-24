bd05fd73ccfbed20600a43980524af37921a5e9e
JBlond <leet31337@web.de>
2021-08-24 21:27:14 +0200

    correct typo

--- build_apache.sh
+++ build_apache.sh
@@ -128,7 +128,7 @@ export LDFLAGS="-Wl,-rpath,/opt/openssl/lib"
 ./configure --prefix=/opt/apache2 --enable-pie --enable-mods-shared=all --enable-so --disable-include --enable-lua --enable-deflate \
 	--enable-headers --enable-expires --with-curl=/opt/curl --enable-http2 --with-nghttp2=/opt/nghttp2 --enable-proxy-http2 \
 	--enable-ssl=shared --with-ssl=/opt/openssl --with-openssl=/opt/openssl --with-crypto --enable-module=ssl \
-	----with-apr-util=${HOME}/apache24/httpd-${HTTPD_VERSION}/srclib/apr-util \
+	--with-apr-util=${HOME}/apache24/httpd-${HTTPD_VERSION}/srclib/apr-util \
 	--enable-mpms-shared=all --with-mpm=event --enable-rewrite --enable-md --with-z=${HOME}/apache24/httpd-${HTTPD_VERSION}/srclib/zlib --enable-fcgid \
 	--with-included-apr --enable-nonportable-atomics=yes
 make
