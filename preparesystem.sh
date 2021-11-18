fe86d39057876b1575a37e5e3894fadc73653080
JBlond <leet31337@web.de>
2021-11-18 09:03:40 +0100

    add unzip to needed software

--- preparesystem.sh
+++ preparesystem.sh
@@ -5,7 +5,7 @@ cd $HOME/apache24
 # debian
 which apt  > /dev/null 2>&1 && {
 	sudo apt update || echo apt update hit problems but continuing anyway...
-	sudo apt -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python
+	sudo apt -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python unzip
 }
 
 # centos
