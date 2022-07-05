6be4246cf4b6572c6ddfaa418aedb16080eab1ae
JBlond <leet31337@web.de>
2022-07-05 15:26:40 +0200

    fix remove

--- build_apache.sh
+++ build_apache.sh
@@ -215,7 +215,8 @@ then
 	wget https://github.com/JBlond/mod_bikeshed/archive/refs/tags/1.0.0.tar.gz
 	tar xvfz 1.0.0.tar.gz
 	cd mod_bikeshed-1.0.0/
-	sudo /opt/apache2/bin/apxs -ci mod_bikeshed.c
+	/opt/apache2/bin/apxs -i mod_bikeshed.c
+	sudo /opt/apache2/bin/apxs -i mod_bikeshed.c
 fi
 
 cd "${HOME}/apache24"
