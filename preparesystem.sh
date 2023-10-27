bf45081351143dc33c12da2aebd61c755d11d56f
JBlond <leet31337@web.de>
2023-10-27 09:29:35 +0200

    enable nala if available

--- preparesystem.sh
+++ preparesystem.sh
@@ -2,7 +2,14 @@
 mkdir $HOME/apache24
 cd $HOME/apache24
 
-sudo apt update || echo apt update hit problems but continuing anyway...
-sudo apt -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc
+if command -v nala &> /dev/null
+then
+    icmd='nala'
+else
+    icmd='apt'
+fi
+
+sudo $icmd update || echo $icmd update hit problems but continuing anyway...
+sudo $icmd -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc
 
 exit 0
