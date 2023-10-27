43995d744b88e8e29f652fcf5f70521ff7a598ec
JBlond <leet31337@web.de>
2023-10-27 22:58:04 +0200

    Update prepare script

--- preparesystem.sh
+++ preparesystem.sh
@@ -4,12 +4,14 @@ cd $HOME/apache24
 
 if command -v nala &> /dev/null
 then
-    icmd='nala'
+    sudo nala update || echo nala update hit problems but continuing anyway...
+    sudo nala install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc
 else
-    icmd='apt'
+    sudo apt update || echo apt update hit problems but continuing anyway...
+    sudo apt -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc
 fi
 
-sudo $icmd update || echo $icmd update hit problems but continuing anyway...
-sudo $icmd -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre3-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc
+
+
 
 exit 0
