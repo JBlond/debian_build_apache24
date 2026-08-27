24053629a7f8acf6d75b0f4f67a872a584ebc55f
JBlond <leet31337@web.de>
2026-08-27 14:00:39 +0200

    update

--- preparesystem.sh
+++ preparesystem.sh
@@ -5,10 +5,10 @@ cd $HOME/apache24
 if command -v nala &> /dev/null
 then
     sudo nala update || echo nala update hit problems but continuing anyway...
-    sudo nala install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre2-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc libzstd-dev ccache
+    sudo nala install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre2-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc libzstd-dev ccache libtool-bin
 else
     sudo apt update || echo apt update hit problems but continuing anyway...
-    sudo apt -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre2-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc libzstd-dev ccache
+    sudo apt -y install wget build-essential zlib1g-dev liblua5.1-dev autoconf libtool libpcre2-dev libxml2-dev libexpat1-dev libcurl4-openssl-dev libyajl-dev brotli python3 unzip gcc libzstd-dev ccache libtool-bin
 fi
 
 exit 0
